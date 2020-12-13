e841ab0242d8a61223705e29af314faa80cdd6b3
Aimee Mattson <you@example.com>
2020-12-12 18:27:19 -0800

    added style to textarea

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 
 // for (let day = 1; day <=31; day = day++);
 // console.log(day)
+
+
+// elements needed-----pulled from HTML
+
+// var data-time=  
+// this will need to equal the user input. so when the user puts in 9 into the form, the var data-timeofdayele will also need to match 9. So ill need a if statement to make sure they match.
+
+// var nineEl=document.querySelector("#9:00");
