f3b9a99891074acd83c656eccc2dd52783a55d84
Aimee Mattson <you@example.com>
2020-12-15 15:14:21 -0800

    created global time element variables

--- script.js
+++ script.js
@@ -9,4 +9,22 @@
 // var data-time=  
 // this will need to equal the user input. so when the user puts in 9 into the form, the var data-timeofdayele will also need to match 9. So ill need a if statement to make sure they match.
 
-// var nineEl=document.querySelector("#9:00");
+
+
+// Function allows JS to run before HTML is loaded.
+$( document ).ready(function() {})
+
+
+
+// Global variables
+// Hours of the day variables
+
+var nineHourEl=document.querySelector("#0900hour");
+var tenHourEl=document.querySelector("#1000hour");
+var elevenHourEl=document.querySelector("#1100hour");
+var twelveHourEl=document.querySelector("#1200hour");
+var thirteenHourEl=document.querySelector("#1300hour");
+var fourteenHourEl=document.querySelector("#1400hour");
+var fifteenHourEl=document.querySelector("#1500hour");
+var sixteenHourEl=document.querySelector("#1600hour");
+var seventeenHourEl=document.querySelector("#1700hour");
